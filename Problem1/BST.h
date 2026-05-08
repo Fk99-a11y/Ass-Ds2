c58a8bf99c3b9c61b2139bbaa137e44365075a7f
Fk99-a11y <fk080641@gmail.com>
2026-05-08 21:13:18 +0300

    Update BST.h

--- Problem1/BST.h
+++ Problem1/BST.h
@@ -1,1 +1,90 @@
+#ifndef BST_H
+#define BST_H
 
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+// ================= BOOK STRUCT =================
+
+// Book information
+struct Book {
+    int id;
+    string title;
+    string author;
+};
+
+// ================= NODE STRUCT =================
+
+// Tree node
+struct Node {
+
+    Book data;
+
+    Node* left;
+    Node* right;
+
+    // Constructor
+    Node(Book b) {
+        data = b;
+        left = right = nullptr;
+    }
+};
+
+// ================= BST CLASS =================
+
+class BST {
+
+private:
+
+    Node* root;
+
+    // Core BST operations
+    Node* insert(Node* node, Book book);
+
+    Node* deleteNode(Node* node, int id);
+
+    Node* findMin(Node* node);
+
+    Node* search(Node* node, int id, int& steps);
+
+    // Traversal
+    void inorder(Node* node);
+
+    // Tree height
+    int height(Node* node);
+
+    // Additional features
+    void rangeSearch(Node* node, int min, int max);
+
+    void closestID(Node* node, int target, int& closest);
+
+public:
+
+    // Constructor
+    BST();
+
+    // Insert new book
+    void insert(Book book);
+
+    // Delete book by ID
+    void deleteBook(int id);
+
+    // Search for book by ID
+    void searchBook(int id);
+
+    // Display books in sorted order
+    void displayBooks();
+
+    // Return tree height
+    int getHeight();
+
+    // Display books within range
+    void findBooksInRange(int min, int max);
+
+    // Find closest book ID
+    void findClosestBook(int target);
+};
+
+#endif
