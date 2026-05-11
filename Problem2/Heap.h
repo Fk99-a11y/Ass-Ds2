23272aea0bc81cca10facfdca7c462ff37137437
youssefamir05 <youssefamir878@gmail.com>
2026-05-12 00:50:09 +0300

    Add EmergencyHeap class for patient management

--- Problem2/Heap.h
+++ Problem2/Heap.h
@@ -1,1 +1,44 @@
+#ifndef HEAP_H
+#define HEAP_H
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Patient {
+    int id;
+    string name;
+    int severity;
+    int arrivalTime;
+};
+
+class EmergencyHeap {
+
+private:
+
+    vector<Patient> heap;
+
+    int parent(int i);
+    int leftChild(int i);
+    int rightChild(int i);
+
+    bool higherPriority(Patient a, Patient b);
+
+    void heapifyUp(int index);
+    void heapifyDown(int index);
+
+public:
+
+    void insertPatient(Patient p);
+
+    void treatNextPatient();
+
+    void viewNextPatient();
+
+    void updateSeverity(int id, int newSeverity);
+
+    void displayPatients();
+};
+
+#endif
